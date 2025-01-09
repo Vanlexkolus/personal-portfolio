ebd0f98cbcc5f7d10f4d23241585f322c337a059
alexander kolurejo <kolurejoalexanderola@gmail.com>
2025-01-09 01:18:24 +0100

    added email links and globe animation

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "HTML", "Typescript"];
+  const rightLists = ["VueJS", "NextJS", "CSS"];
 
   const [copied, setCopied] = useState(false);
 
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "kolurejoalexanderola@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
--- data/index.ts
+++ data/index.ts
@@ -107,7 +107,19 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Alexnder was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Alex's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Alex is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Alexander was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Alex's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Alex is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Alexander was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Alex's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Alex is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
@@ -119,19 +131,7 @@ export const testimonials = [
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Alexander was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Alex's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Alex is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
